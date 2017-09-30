3f9137675a4251883ec74bd58fe594d33563eeee
Alexander Gomeniuk <thisisbanana@hotmail.com>
2017-10-01 01:55:46 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MelazWeb
+Heat change model
++ Web
++ UI
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 30.09.2017
+ * Time: 23:47
+ */ ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Модель остывания чашки кофе</title>
+    <style>
+        iframe {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            min-width: 100%;
+            min-height: 100%;
+            width: auto;
+            height: auto;
+            z-index: -100;
+            transform: translateX(-50%) translateY(-50%);
+            transition: 1s opacity;
+        }
+    </style>
+</head>
+<body>
+<figure>
+    <iframe width="1024" height="700" src="https://www.youtube.com/embed/mHTx0mhRFws?autoplay=1" frameborder="0"
+            allowfullscreen></iframe>
+</figure>
+</body>
+</html>
