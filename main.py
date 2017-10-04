d15042f0a37f00482a961f70d4bfad473edb750e
Alexander Gomeniuk <thisisbanana@hotmail.com>
2017-10-04 22:30:19 +1000

    Change output format to Google Charts compatible
    Use it via passing -gc argument

---
+++ main.py
@@ -0,0 +1,46 @@
+from models import CoolingProcessCalculation as CPC
+import argparse
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gc', '--charts', help='google charts compatible json', action='store_true')
+    parser.add_argument('-an', '--analytical', help='use analytical method', action='store_true')
+    parser.add_argument('-eu', '--euler', help='use Euler method', action='store_true')
+    parser.add_argument('-en', '--enhanced', help='use enhanced Euler method', action='store_true')
+    parser.add_argument('-rk', '--RK4', help='use 4th order Runge--Kutta method', action='store_true')
+    parser.add_argument('-cc', '--coefficient', help='thermal conductivity coefficient', type=float, metavar='')
+    parser.add_argument('-ct', '--coffee', help='coffee temperature', type=float, metavar='')
+    parser.add_argument('-et', '--environment', help='environment temperature', type=float, metavar='')
+    parser.add_argument('-tr', '--time', help='time range', type=int, metavar='')
+    parser.add_argument('-sc', '--segments', help='segments count', type=int, metavar='')
+    args = parser.parse_args()
+
+    parameters, methods = {}, []
+    if args.coefficient:
+        parameters['CoolingCoefficient'] = args.coefficient
+    if args.coffee:
+        parameters['BaseTemperature'] = args.coffee
+    if args.environment:
+        parameters['EnvironmentTemperature'] = args.environment
+    if args.time:
+        parameters['TimeRange'] = args.time
+    if args.segments:
+        parameters['SegmentsCount'] = args.segments
+    if args.analytical:
+        methods.append('analytical')
+    if args.euler:
+        methods.append('euler')
+    if args.enhanced:
+        methods.append('euler_enhanced')
+    if args.RK4:
+        methods.append('RK4')
+
+    data, labels, cpc = [], ['methods'], CPC(parameters)
+    print('{\n"data":')
+    for x in methods if len(methods) else CPC.defaultMethods:
+        labels.append(x)
+        data.append(eval(''.join(("cpc.", x, "()"))))
+    if args.charts:
+        data = [labels, *([str(key), *cpc.googleLineChartFormattedData[key]] for key in cpc.googleLineChartFormattedData)]
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')), '\n}')
---
+++ models.py
@@ -0,0 +1,73 @@
+from math import exp
+
+
+class CoolingProcessCalculation:
+    defaultMethods = [
+        'analytical'
+    ]
+
+    defaultParameters = {
+        'BaseTemperature': 80.0,
+        'EnvironmentTemperature': 40.0,
+        'CoolingCoefficient': 0.5,
+        'TimeRange': 10,
+        'SegmentsCount': 20
+    }
+
+    def __init__(self, parameters):
+        parameters = {**self.defaultParameters, **parameters}
+        self.BaseTemperature = parameters['BaseTemperature']
+        self.EnvironmentTemperature = parameters['EnvironmentTemperature']
+        self.CoolingCoefficient = parameters['CoolingCoefficient']
+        self.SegmentsCount = parameters['SegmentsCount']
+        self.Step = parameters['TimeRange'] / parameters['SegmentsCount']
+        self.TemperatureDifference = self.BaseTemperature - self.EnvironmentTemperature
+        self.googleLineChartFormattedData = {k:[] for k in [self.Step * i for i in range(0, self.SegmentsCount)]}
+
+    def _nloc(self, t):
+        return -self.CoolingCoefficient * (t - self.EnvironmentTemperature)
+
+    def _nloc_solution(self, t):
+        return self.EnvironmentTemperature + exp(-t * self.CoolingCoefficient) * self.TemperatureDifference
+
+    def analytical(self):
+        result = {'Analytical': {0: self.BaseTemperature}}
+        self.googleLineChartFormattedData[0].append(self.BaseTemperature)
+        for i in range(1, self.SegmentsCount):
+            result['Analytical'][self.Step * i] = self._nloc_solution(self.Step * i)
+            self.googleLineChartFormattedData[self.Step * i].append(result['Analytical'][self.Step * i])
+        return result
+
+    def euler(self):
+        result = {'Euler': {0: self.BaseTemperature}}
+        self.googleLineChartFormattedData[0].append(self.BaseTemperature)
+        for i in range(0, self.SegmentsCount - 1):
+            result_i = result['Euler'][self.Step * i]
+            result['Euler'][self.Step * (i + 1)] = result_i + self.Step * self._nloc(result_i)
+            self.googleLineChartFormattedData[self.Step * (i + 1)].append(result['Euler'][self.Step * (i + 1)])
+        return result
+
+    def euler_enhanced(self):
+        result = {'Euler Enhanced': {0: self.BaseTemperature}}
+        self.googleLineChartFormattedData[0].append(self.BaseTemperature)
+        for i in range(0, self.SegmentsCount - 1):
+            result_i = result['Euler Enhanced'][self.Step * i]
+            k = result_i + self.Step * self._nloc(result_i)
+            result['Euler Enhanced'][self.Step * (i + 1)] = result_i + self.Step * (self._nloc(result_i) +
+                self._nloc(k)) / 2.0
+            self.googleLineChartFormattedData[self.Step * (i + 1)].append(result['Euler Enhanced'][self.Step * (i + 1)])
+        return result
+
+    def RK4(self):
+        result = {'RK4': {0: self.BaseTemperature}}
+        self.googleLineChartFormattedData[0].append(self.BaseTemperature)
+        for i in range(0, self.SegmentsCount - 1):
+            result_i = result['RK4'][self.Step * i]
+            k1 = self._nloc(result_i)
+            k2 = self._nloc(result_i + self.Step * k1 / 2.0)
+            k3 = self._nloc(result_i + self.Step * k2 / 2.0)
+            k4 = self._nloc(result_i + self.Step * k3)
+            k = result_i + self.Step * self._nloc(result_i)
+            result['RK4'][self.Step * (i + 1)] = result_i + self.Step * (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+            self.googleLineChartFormattedData[self.Step * (i + 1)].append(result['RK4'][self.Step * (i + 1)])
+        return result
